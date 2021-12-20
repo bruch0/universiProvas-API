a1992420cb3d437fb6ca344ed1465af0de8e6e81
bruch0 <lucas.bruch0@gmail.com>
2021-12-20 11:42:48 -0300

    feat: add service

--- src/services/testService.ts
+++ src/services/testService.ts
@@ -3,12 +3,21 @@ import dayjs from "dayjs";
 
 import { Professors } from "../entities/professors";
 import { Subjects } from "../entities/subjects";
+import { uploadFileS3 } from "./aws/uploadFile";
+import { Tests } from "../entities/tests";
+import { TestTypes } from "../entities/testTypes";
 
 const getTestNeededInfo = async () => {
   const professors = await getConnection()
     .createQueryBuilder()
     .select(["professors.name as name", "professors.id as id"])
     .from(Professors, "professors")
+    .execute();
+
+  const testTypes = await getConnection()
+    .createQueryBuilder()
+    .select(["test_types.name as name", "test_types.id as id"])
+    .from(TestTypes, "test_types")
     .execute();
 
   const subjects = await getConnection()
@@ -33,7 +42,57 @@ const getTestNeededInfo = async () => {
       name: `${dayjs(minimumPeriod).add(3, "years").format("YYYY")}.1`,
     });
 
-  return { professors, subjects, availablePeriods };
+  return { professors, subjects, testTypes, availablePeriods };
 };
 
-export { getTestNeededInfo };
+const uploadTest = async (file: any, filename: string) => {
+  const fileUrl = await uploadFileS3(file, filename);
+
+  return fileUrl;
+};
+
+const checkTestParams = async (professorId: number, subjectId: number) => {
+  const professor = await getConnection()
+    .createQueryBuilder()
+    .select("professors.name as name")
+    .from(Professors, "professors")
+    .where(`professors.id = ${professorId}`)
+    .execute();
+
+  const subject = await getConnection()
+    .createQueryBuilder()
+    .select("subjects.name as name")
+    .from(Subjects, "subjects")
+    .where(`subjects.id = ${subjectId}`)
+    .execute();
+
+  return Boolean(professor.length && subject.length);
+};
+
+const registerTestOnDatabase = async (
+  fileUrl: string,
+  professorId: number,
+  subjectId: number,
+  typeId: number,
+  period: string
+) => {
+  await getConnection()
+    .createQueryBuilder()
+    .insert()
+    .into(Tests)
+    .values({
+      professor_id: professorId,
+      subject_id: subjectId,
+      type_id: typeId,
+      year: period,
+      url: fileUrl,
+    })
+    .execute();
+};
+
+export {
+  getTestNeededInfo,
+  uploadTest,
+  checkTestParams,
+  registerTestOnDatabase,
+};
