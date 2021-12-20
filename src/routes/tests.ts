249ee85fc7d9cb407a2f75603b6f07674a1f8832
bruch0 <lucas.bruch0@gmail.com>
2021-12-20 11:43:07 -0300

    feat: add uploading tests to the router

--- src/routes/tests.ts
+++ src/routes/tests.ts
@@ -1,8 +1,11 @@
 import express from "express";
+
 import * as testController from "../controllers/testController";
 
 const router = express.Router();
 
 router.get("/tests", testController.getCourseSubjects);
+
+router.post("/tests", testController.uploadTest);
 
 export default router;
